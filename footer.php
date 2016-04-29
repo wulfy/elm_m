718c071e01afe0c5ab3346afd42773903ceee79e
Ludovic Lasry <llasry@ludo-mac.local>
2016-04-29 22:44:25 +0200

    add GA tracker

--- footer.php
+++ footer.php
@@ -27,7 +27,21 @@ $( document ).ready(function(){
 	searchBox.focusout(function() {$(this).removeClass("animate")});
 	searchbutton.click(function() {searchBox.toggleClass("animate"); (searchBox.is(":focus"))?'':searchBox.focus();});
 })
+
+
 </script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71697241-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <footer class="page-footer grey">
           <div class="container">
             <div class="row">
